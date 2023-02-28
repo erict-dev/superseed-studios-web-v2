acaabe8e6e98a335171bf6665fc8abc38710f44a
erict-dev <4564093+erict-dev@users.noreply.github.com>
2023-02-27 16:11:30 -0800

    migrated home index page

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,70 +1,41 @@
 ---
 templateKey: index-page
-title: Great coffee with a conscience
-image: /img/home-jumbotron.jpg
-heading: Great coffee with a conscience
-subheading: Support sustainable farming while enjoying a cup
+title: Superseed Studios
+featuredVideoPoster: /img/bossfight.jpg
+featuredVideo: /img/riverbond-cut-3.mp4
 mainpitch:
-  title: Why Kaldi
   description: >
-    Kaldi is the coffee store for everyone who believes that great coffee
-    shouldn't just taste good, it should do good too. We source all of our beans
-    directly from small scale sustainable farmers and make sure part of the
-    profits are reinvested in their communities.
-description: >-
-  Kaldi is the ultimate spot for coffee lovers who want to learn about their
-  java’s origin and support the farmers that grew it. We take coffee production,
-  roasting and brewing seriously and we’re glad to pass that knowledge to
-  anyone.
-intro:
-  blurbs:
-    - image: /img/coffee.png
-      text: >
-        We sell green and roasted coffee beans that are sourced directly from
-        independent farmers and farm cooperatives. We’re proud to offer a
-        variety of coffee beans grown with great care for the environment and
-        local communities. Check our post or contact us directly for current
-        availability.
-    - image: /img/coffee-gear.png
-      text: >
-        We offer a small, but carefully curated selection of brewing gear and
-        tools for every taste and experience level. No matter if you roast your
-        own beans or just bought your first french press, you’ll find a gadget
-        to fall in love with in our shop.
-    - image: /img/tutorials.png
-      text: >
-        Love a great cup of coffee, but never knew how to make one? Bought a
-        fancy new Chemex but have no clue how to use it? Don't worry, we’re here
-        to help. You can schedule a custom 1-on-1 consultation with our baristas
-        to learn anything you want to know about coffee roasting and brewing.
-        Email us or call the store for details.
-    - image: /img/meeting-space.png
-      text: >
-        We believe that good coffee has the power to bring people together.
-        That’s why we decided to turn a corner of our shop into a cozy meeting
-        space where you can hang out with fellow coffee lovers and learn about
-        coffee making techniques. All of the artwork on display there is for
-        sale. The full price you pay goes to the artist.
-  heading: What we offer
+    We are a remote animation studio that specializes in gameplay and cinematic
+    animations. We pride ourselves on the quality of our work and the level of
+    communication we have with our clients. With our wide breadth of animation
+    experience, we can handle your animation project and bring your concepts to
+    life. From video games to film, commercials to music videos.
+  description2: >
+    What sets us apart is our high standards of quality and the in-depth
+    services that we provide. An example of one of our specialties is providing
+    high fidelity, stylized work on the gameplay side for our projects. This
+    includes implementation of the animations right into your game engine and
+    iterating with you to provide gameplay animation solutions.
+  title: We bring your ideas to life
+secondarypitch:
   description: >
-    Kaldi is the ultimate spot for coffee lovers who want to learn about their
-    java’s origin and support the farmers that grew it. We take coffee
-    production, roasting and brewing seriously and we’re glad to pass that
-    knowledge to anyone. This is an edit via identity...
-main:
-  heading: Great coffee with no compromises
-  description: >
-    We hold our coffee to the highest standards from the shrub to the cup.
-    That’s why we’re meticulous and transparent about each step of the coffee’s
-    journey. We personally visit each farm to make sure the conditions are
-    optimal for the plants, farmers and the local environment.
-  image1:
-    alt: A close-up of a paper filter filled with ground coffee
-    image: /img/products-grid3.jpg
-  image2:
-    alt: A green cup of a coffee on a wooden table
-    image: /img/products-grid2.jpg
-  image3:
-    alt: Coffee beans
-    image: /img/products-grid1.jpg
+    Our breadth of expertise allows us to adapt to a wide range of different
+    animation projects and requests that our clients have. We've worked
+    successfully with many industry leaders in the video game and animation
+    space including: Epic Games, Blizzard Entertainment, Supercell, Oddworld,
+    Outfit 7, Ganymede Games, Main Road Post VFX, Survios, and White Elk game
+    studios. Outside of the gaming realm, we've animated promos and commercials
+    for brands including Pepsi and Tokyo 2020. We consistently meet and exceed
+    the quality bar that our clients expect. Our animation work brings your
+    stories and ideas to life and has impacted millions of players, fans, and
+    customers.
+  secondaryVideo: /img/superseed_landingpage_bottom.mp4
+  secondaryVideoPoster: /img/superseed_landingpage_bottom_image.jpg
+  title: Our passion for animation and storytelling
+featuredClient1: /img/pepsi.png
+featuredClient2: /img/pepsi.png
+featuredClient3: /img/blizzard.png
+featuredClient4: /img/supercell.png
+featuredClient5: /img/outfit7.png
+featuredClient6: /img/oddworld.png
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,108 +1,140 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
 
-import Layout from "../components/Layout";
-import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
-import FullWidthImage from "../components/FullWidthImage";
+import Layout from '../components/Layout'
+import Services from '../components/Services'
 
-// eslint-disable-next-line
 export const IndexPageTemplate = ({
-  image,
+  featuredVideoPoster,
+  featuredVideo,
   title,
-  heading,
-  subheading,
   mainpitch,
-  description,
-  intro,
-}) => {
-  const heroImage = getImage(image) || image;
-
-  return (
-    <div>
-      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
-      <section className="section section--gradient">
-        <div className="container">
-          <div className="section">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="content">
-                  <div className="content">
-                    <div className="tile">
-                      <h1 className="title">{mainpitch.title}</h1>
-                    </div>
-                    <div className="tile">
-                      <h3 className="subtitle">{mainpitch.description}</h3>
-                    </div>
-                  </div>
-                  <div className="columns">
-                    <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2">
-                        {heading}
-                      </h3>
-                      <p>{description}</p>
-                    </div>
-                  </div>
-                  <Features gridItems={intro.blurbs} />
-                  <div className="columns">
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
-                        See all products
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      Latest stories
-                    </h3>
-                    <BlogRoll />
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/blog">
-                        Read more
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  secondarypitch,
+  featuredClient1,
+  featuredClient2,
+  featuredClient3,
+  featuredClient4,
+  featuredClient5,
+  featuredClient6,
+}) => (
+  <div>
+    <div class="hero is-medium" style={{ position: 'relative' }}>
+        <video
+          className="hero-video-cover"
+          style={{  objectFit: 'cover', width: '100%', height: '100%', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundImage: `url(${featuredVideoPoster.childImageSharp.fluid.src})`}}
+          src={featuredVideo.publicURL}
+          autoPlay
+          muted
+          loop
+        />
+      <section class="hero-body">
+        <div class="container hero-title-container has-text-centered">
+          <div className="hero-title">Superseed Studios</div>
         </div>
       </section>
     </div>
-  );
-};
+
+    <section className="container">
+      <div className="section-spacer" />
+      <div class="columns is-desktop">
+        <div class="column">
+          <div className="pitch-content-container">
+            <div className="pitch-title">{mainpitch.title}</div>
+            <div className="pitch-description">
+              <p>{mainpitch.description}</p>
+              <br />
+              <p>{mainpitch.description2}</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section className="container">
+      <div className="columns">
+        <div className="column">
+          <div className="pitch-content-container" style={{}}>
+            <div className="pitch-title">{secondarypitch.title}</div>
+            <div className="pitch-description">{secondarypitch.description}</div>
+
+            <div className="clients-container">
+              <img alt="" className="client-img" src={featuredClient1.childImageSharp.fluid.src}/>
+              <img alt="" className="client-img" src={featuredClient2.childImageSharp.fluid.src}/>
+              <img alt="" className="client-img" src={featuredClient3.childImageSharp.fluid.src}/>
+              <img alt="" className="client-img" src={featuredClient4.childImageSharp.fluid.src}/>
+              <img alt="" className="client-img" src={featuredClient5.childImageSharp.fluid.src}/>
+              <img alt="" className="client-img" src={featuredClient6.childImageSharp.fluid.src}/>
+            </div>
+
+          </div>
+          <div>
+            <video
+              style={{ objectFit: 'cover', width: '100%', height: '100%', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundImage: `url(${secondarypitch.secondaryVideoPoster.childImageSharp.fluid.src})`}}
+              src={secondarypitch.secondaryVideo.publicURL}
+              autoPlay
+              muted
+              loop
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div className="section-spacer" />
+
+    <section className="container">
+        <div className="bottom-cta">
+          <div className="pitch-title" style={{ textAlign: 'center', maxWidth: '600px' }}>We can help you with your next animation project</div>
+          <Services />
+          <Link to="/contact"><button className="cta-button button is-light is-outlined is-rounded">Contact</button></Link>
+        </div>
+    </section>
+
+    <div className="section-spacer" style={{ height: 20 }}/>
+
+  </div>
+)
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  featuredVideo: PropTypes.object,
+  featuredVideoPoster: PropTypes.object,
   title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
   mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
+  secondarypitch: PropTypes.object,
+  featuredClient1: PropTypes.object,
+  featuredClient2: PropTypes.object,
+  featuredClient3: PropTypes.object,
+  featuredClient4: PropTypes.object,
+  featuredClient5: PropTypes.object,
+  featuredClient6: PropTypes.object,
+  /*intro: PropTypes.shape({
     blurbs: PropTypes.array,
-  }),
-};
+  }),*/
+}
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark
 
   return (
-    <Layout>
+    <Layout navTitle="Superseed Studios">
       <IndexPageTemplate
-        image={frontmatter.image}
+        featuredVideo={frontmatter.featuredVideo}
+        featuredVideoPoster={frontmatter.featuredVideoPoster}
         title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
         mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        secondarypitch={frontmatter.secondarypitch}
+        featuredClient1={frontmatter.featuredClient1}
+        featuredClient2={frontmatter.featuredClient2}
+        featuredClient3={frontmatter.featuredClient3}
+        featuredClient4={frontmatter.featuredClient4}
+        featuredClient5={frontmatter.featuredClient5}
+        featuredClient6={frontmatter.featuredClient6}
       />
     </Layout>
-  );
-};
+  )
+}
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -110,40 +142,86 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-};
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
+        featuredVideo {
+          publicURL
+        }
+        featuredVideoPoster {
           childImageSharp {
-            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
-        heading
-        subheading
         mainpitch {
           title
           description
+          description2
         }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
+        secondarypitch {
+          title
+          description
+          secondaryVideoPoster {
+            childImageSharp {
+              fluid(maxWidth: 800, quality: 100) {
+                ...GatsbyImageSharpFluid
               }
             }
-            text
           }
-          heading
-          description
+          secondaryVideo {
+            publicURL
+          }
+        }
+        featuredClient1 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        featuredClient2 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        featuredClient3 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        featuredClient4 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        featuredClient5 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        featuredClient6 {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
-  }
-`;
+  }`
